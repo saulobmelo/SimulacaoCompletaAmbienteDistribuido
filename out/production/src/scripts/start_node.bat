90db1ff055b2a919f05dbb70e0a7ed7a7d0f15d6
SaulinhoDevs <165098314+saulobmelo@users.noreply.github.com>
2025-08-21 16:22:39 -0300

    Add PARES no argumento do start_node.bat script

--- out/production/src/scripts/start_node.bat
+++ out/production/src/scripts/start_node.bat
@@ -1,12 +1,14 @@
 @echo off
-REM Uso: start_node NODEID GROUP PORT
+REM Uso: start_node NODEID GROUP PORT PARES
 set NODE=%1
 set GROUP=%2
 set PORT=%3
+set PARES=%4
 
 if "%NODE%"=="" set NODE=A1
 if "%GROUP%"=="" set GROUP=A
 if "%PORT%"=="" set PORT=5000
+if "%PARES%"=="" set PARES=""
 
-echo Iniciando nó %NODE% grupo %GROUP% porta %PORT%...
-start cmd /k java -cp out Main %NODE% %GROUP% %PORT%
+echo Iniciando nó %NODE% grupo %GROUP% porta %PORT% pares %PARES%...
+java -cp out Main %NODE% %GROUP% %PORT% %PARES%
