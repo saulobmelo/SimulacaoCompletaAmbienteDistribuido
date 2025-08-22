d23908da6f6de5fd4b8bb3cd1e44f17ec6aae48c
SaulinhoDevs <165098314+saulobmelo@users.noreply.github.com>
2025-08-21 21:00:20 -0300

    att

--- src/src/Main.java
+++ src/src/Main.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws Exception {
         // args: nodeId grupo porta listaPares
-        // Exemplo: A1 A 5001 A2:5002,A3:5003
         String nodeId = args.length > 0 ? args[0] : "A1";
         String grupo = args.length > 1 ? args[1] : "A";
         int tcpPort = args.length > 2 ? Integer.parseInt(args[2]) : 5000;
